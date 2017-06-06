15e7e77d3fa8dbf57096a1364433f8f21baaa23c
hohayo <hohayo@gmail.com>
2017-06-06 23:52:15 +0800

    modify

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class TasksController extends Controller
 {
@@ -23,7 +24,7 @@ class TasksController extends Controller
      */
     public function create()
     {
-        //
+        return view('tasks.create');
     }
 
     /**
@@ -34,7 +35,7 @@ class TasksController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        return 'hello';
     }
 
     /**
---
+++ resources/views/tasks/create.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>任務新增</title>
+</head>
+<body>
+	<form action="../tasks" method="post">
+		<input type="text" name="title">
+		<input type="submit" value="新增任務">
+	</form>
+</body>
+</html>
